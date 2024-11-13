45fb04ddcc92dc69042840fe4882ef02efb6bb90
SekianZ <sebasb256@gmail.com>
2024-11-12 20:50:52 -0500

    Actualización de configuración en .env y cambios en opciones.php

--- backend/opciones.php
+++ backend/opciones.php
@@ -10,10 +10,19 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$nomDB = $_ENV['DATABASE_NAME'] ;
-$User =$_ENV['DATABASE_USER'] ;
-$host = $_ENV['DATABASE_HOST'] ;
-$contraseña =$_ENV['DATABASE_PASSWORD'] ;
+$databaseUrl = $_ENV['DATABASE_URL'];
+
+// Utiliza parse_url() para descomponer la URL
+$parsedUrl = parse_url($databaseUrl);
+
+// Parsear la URL de la base de datos
+$url = parse_url($dbUrl);
+
+// Extraer los datos de la URL de la base de datos
+$host = $url['host'];
+$usuario = $url['user'];
+$contraseña = $url['pass'];
+$nombreBaseDatos = ltrim($url['path'], '/');  // Elimina el '/' al principio de la ruta
 
 header('Content-Type: application/json');
 
